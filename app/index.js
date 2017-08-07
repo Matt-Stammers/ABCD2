16e8f9808808bb90ccc31129a76a430ba0fa8123
Matt Stammers <matthewstammers@googlemail.com>
2017-08-07 20:58:30 +0100

    Add files via upload

--- app/index.js
+++ app/index.js
@@ -1,16 +1,33 @@
 var values = {};
 
 $( document ).ready(function() {
-  $( "#submit" ).click(function(event) {
-    event.preventDefault();
 
+  $("input").change(function() {
     $.each($('#abcd-form').serializeArray(), function(i, field) {
       values[field.name] = field.value;
     });
 
     var patientScore = abcd2risk(parseInt(values.age,10), parseInt(values.systolicBP, 10), parseInt(values.diastolicBP, 10), values.clinicalFeatures, parseInt(values.duration, 10), values.diabetes)
-    console.log(patientScore)
 
     $("#patientScore").text(patientScore)
-});
+
+    changeRiskBoxes(patientScore)
+  });
+
 })
+
+function changeRiskBoxes(patientScore) {
+  if ( patientScore >= 6 ) {
+    $("#lowRisk").css('display', 'none')
+    $("#mediumRisk").css('display', 'none')
+    $("#highRisk").css('display', 'inline-block')
+  } else if (patientScore <= 3) {
+    $("#lowRisk").css('display', 'inline-block')
+    $("#mediumRisk").css('display', 'none')
+    $("#highRisk").css('display', 'none')
+  } else {
+    $("#lowRisk").css('display', 'none')
+    $("#mediumRisk").css('display', 'inline-block')
+    $("#highRisk").css('display', 'none')
+  }
+}
